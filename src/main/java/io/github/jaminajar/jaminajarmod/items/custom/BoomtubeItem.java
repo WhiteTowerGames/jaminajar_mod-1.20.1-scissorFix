7de551e9bdc8ca7eec3b2a5764bb3a15726dcd09
JamInAJar <jaminajar.eee@gmail.com>
2025-02-16 00:13:25 +1100

    1

--- src/main/java/io/github/jaminajar/jaminajarmod/items/custom/BoomtubeItem.java
+++ src/main/java/io/github/jaminajar/jaminajarmod/items/custom/BoomtubeItem.java
@@ -9,57 +9,88 @@ import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.ToolItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.*;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.UseAction;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class BoomtubeItem extends ToolItem {
     private final int maxGunpowder;
-    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
-    public BoomtubeItem(ToolMaterial toolMaterial, Settings settings, int maxGunpowder) {
+    public BoomtubeItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings, int maxGunpowder) {
         super(toolMaterial, settings);
         this.maxGunpowder = maxGunpowder;
-        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(
-                EntityAttributes.GENERIC_ATTACK_DAMAGE,
-                new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Tool modifier", 8.0, EntityAttributeModifier.Operation.ADDITION)
-        );
-        builder.put(
-                EntityAttributes.GENERIC_ATTACK_SPEED,
-                new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Tool modifier", -2.9F, EntityAttributeModifier.Operation.ADDITION)
-        );
-        this.attributeModifiers = builder.build();
     }
+
+    public ActionResult useOnBlock(PlayerEntity player, Hand hand){
+        if(player.isSneaking()){
+            ItemStack stack = player.getStackInHand(hand);
+            int currentGunpowder = getGunpowder(stack);
+            if(currentGunpowder<maxGunpowder&&playerHasItem(player,Items.GUNPOWDER)){
+                setGunpowder(stack, currentGunpowder+1);
+                reduceItem(player,Items.GUNPOWDER,1);
+                return ActionResult.SUCCESS;
+            }
+            return ActionResult.PASS;
+        }
+        return ActionResult.FAIL;
+    }
+
     public int getGunpowder(ItemStack stack){
 
         return stack.getOrCreateNbt().getInt("Gunpowder");
     }
-
-
-
     public void setGunpowder(ItemStack stack, int storedGunpowder){
         stack.getOrCreateNbt().putInt("Gunpowder", MathHelper.clamp(storedGunpowder,0,maxGunpowder));
     }
     public int getMaxGunpowder(){
         return maxGunpowder;
     }
+    public boolean isItemBarVisible(ItemStack stack) {
+        return getGunpowder(stack) > 0;  // Show the bar if charge is greater than 0
+    }
 
+    // Step 2: Get the fill amount of the bar (float from 0.0 to 1.0)
+    @Override
+    public int getItemBarStep(ItemStack stack) {
+        int charge = getGunpowder(stack);
+        return MathHelper.ceil(charge * 13.0F / maxGunpowder);  // Calculate the bar step (13 steps)
+    }
 
+    // Step 3: Define the color of the item bar (here we return green when full, red when empty)
+    @Override
+    public int getItemBarColor(ItemStack stack) {
+        int charge = getGunpowder(stack);
+        // Return a color: Green when full, Red when empty
+        return charge == 0 ? 0xFF0000 : 0x00FF00; // Red: 0xFF0000, Green: 0x00FF00
+    }
 
-    public ActionResult use(PlayerEntity player, Hand hand){
-        ItemStack stack = player.getStackInHand(hand);
-        int currentGunpowder = getGunpowder(stack);
-        if(currentGunpowder<maxGunpowder){
-            setGunpowder(stack, currentGunpowder+1);
-            return ActionResult.SUCCESS;
+    public static boolean playerHasItem(PlayerEntity player, Item itemToCheck) {
+        // Loop through the player's inventory and check for the item
+        for (ItemStack stack : player.getInventory().main) {
+            if (stack.getItem() == itemToCheck) {
+                return true; // Player has the item
+            }
         }
-        return ActionResult.PASS;
+        return false; // Player does not have the item
+    }
+    public static boolean reduceItem(PlayerEntity player, Item itemToReduce, int amount) {
+        for (ItemStack stack : player.getInventory().main) {
+            if (stack.getItem() == itemToReduce) {
+                int currentCount = stack.getCount();
+                if (currentCount >= amount) {
+                    stack.decrement(amount); // Decrease the count by the specified amount
+                    return true; // Successfully reduced the item
+                } else {
+                    // If the player doesn't have enough of the item
+                    return false;
+                }
+            }
+        }
+        return false; // Item not found in inventory
     }
     public void explodeOnHit(Entity entity, ItemStack stack) {
         if (getGunpowder(stack)>=maxGunpowder){
@@ -75,14 +106,8 @@ public class BoomtubeItem extends ToolItem {
 
     }
 
-    public void onHit(LivingEntity target, ItemStack stack){
+    public void onEntityHit(LivingEntity target, ItemStack stack){
         explodeOnHit(target,stack);
-    }
-    @Override
-    public Text getName(ItemStack stack) {
-        // Append the gunpowder amount to the tool's name
-        super.getName(stack);
-        return Text.literal(" (Gunpowder: " + getGunpowder(stack) + "/" + getMaxGunpowder() + ")");
     }
 
 }
--- src/main/java/io/github/jaminajar/jaminajarmod/items/custom/CacophonyItem.java
+++ src/main/java/io/github/jaminajar/jaminajarmod/items/custom/CacophonyItem.java
@@ -2,69 +2,54 @@ package io.github.jaminajar.jaminajarmod.items.custom;
 
 import io.github.jaminajar.jaminajarmod.entity.HonkProjectileEntity;
 import io.github.jaminajar.jaminajarmod.entity.NoteProjectileEntity;
-import net.minecraft.command.argument.CoordinateArgument;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.RangedWeaponItem;
+import net.minecraft.item.Vanishable;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import java.util.function.Predicate;
 
-import static net.minecraft.client.util.ParticleUtil.spawnParticles;
-
-public class CacophonyItem extends RangedWeaponItem {
+public class CacophonyItem extends Item implements Vanishable {
     public CacophonyItem(Item.Settings settings) {super(settings);
     }
 
     @Override
-    public Predicate<ItemStack> getProjectiles() {
-        return null;
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BRUSH;
     }
-
-    @Override
-    public int getRange() {
-        return 0;
-    }
-
-
-    public TypedActionResult<ItemStack> use(ItemStack stack, World world, PlayerEntity user, Hand hand, ServerWorld serverWorld) {
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand, ServerWorld serverWorld) {
         ItemStack itemStack = user.getStackInHand(hand);
 
-        world.playSound(
-                null,
-                user.getX(),
-                user.getY(),
-                user.getZ(),
-                SoundEvents.ENTITY_SNOWBALL_THROW,
-                SoundCategory.MUSIC,
-                0.5F,
-                0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F)
-        );
         if (!world.isClient) {
-
             int i;
             for (i = 0; i < 600; i++) {
                 NoteProjectileEntity noteProjectileEntity = new NoteProjectileEntity(world, user);
                 noteProjectileEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 3.5F, 2.0F);
                 world.spawnEntity(noteProjectileEntity);
+                world.playSound(
+                        null,
+                        user.getX(),
+                        user.getY(),
+                        user.getZ(),
+                        SoundEvents.ENTITY_WARDEN_SONIC_BOOM,
+                        SoundCategory.MUSIC,
+                        0.5F,
+                        0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F)
+                );
             }
             HonkProjectileEntity honkProjectileEntity = new HonkProjectileEntity(world, user);
             honkProjectileEntity.setVelocity(user,user.getPitch(),user.getYaw(),0.0f,10.0f, 0.2f);
             Vec3d aimPoint = user.getPos().add(10,0,0);
-
-
             for (int o = 1; o < MathHelper.floor(aimPoint.length()) + 7; o++) {
                 serverWorld.spawnParticles(ParticleTypes.SONIC_BOOM,
                         honkProjectileEntity.getX(),
@@ -76,19 +61,23 @@ public class CacophonyItem extends RangedWeaponItem {
                         0.0,
                         0.0);
             }
-
-
-
             user.playSound(SoundEvents.ENTITY_WARDEN_SONIC_BOOM, 3.0F, 1.0F);
             /// play scotland the brave (fredfiles)
+
             user.getItemCooldownManager().set(this, 200 + i);
 
+            return TypedActionResult.success(itemStack);
         }
-        return TypedActionResult.success(itemStack, world.isClient());
 
+        return TypedActionResult.pass(itemStack);
     }
+
+
     public int getMaxUseTime(ItemStack stack) {
         return 600;
     }
 
+
 }
+
+
