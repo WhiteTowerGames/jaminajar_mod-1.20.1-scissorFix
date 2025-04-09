367caa714e3ebeade87aedd5ddc3831825bd4c7d
JamInAJar <jaminajar.eee@gmail.com>
2025-04-09 20:16:12 +1000

    1

--- src/main/java/io/github/jaminajar/jaminajarmod/items/food/ModFoods.java
+++ src/main/java/io/github/jaminajar/jaminajarmod/items/food/ModFoods.java
@@ -9,15 +9,15 @@ public class ModFoods {
                     .hunger(3)
                     .saturationModifier(4)
                     .snack()
-                    .statusEffect(new StatusEffectInstance(SPEED,5),1f)
-                    .statusEffect(new StatusEffectInstance(ABSORPTION,20,1),1f)
+                    .statusEffect(new StatusEffectInstance(SPEED,400),1f)
+                    .statusEffect(new StatusEffectInstance(ABSORPTION,200,1),1f)
                     .build();
     public static final FoodComponent COOKED_MARSHMALLOW =
             new FoodComponent.Builder()
                     .hunger(4)
                     .saturationModifier(8)
                     .snack()
-                    .statusEffect(new StatusEffectInstance(FIRE_RESISTANCE,30),1f)
-                    .statusEffect(new StatusEffectInstance(ABSORPTION,45,3),1f)
+                    .statusEffect(new StatusEffectInstance(FIRE_RESISTANCE,1000),1f)
+                    .statusEffect(new StatusEffectInstance(ABSORPTION,150,3),1f)
                     .build();
 }
