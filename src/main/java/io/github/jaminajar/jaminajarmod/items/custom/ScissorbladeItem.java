8e6d6e14d2f95d35e496c88776c0f04652f0f00d
WhiteTowerGames <whitetowersoftware@gmail.com>
2025-04-09 14:35:25 +0300

    Enhance ScissorbladeItem to mirror SwordItem's behavior with attribute modifiers and improve method conditions

--- src/main/java/io/github/jaminajar/jaminajarmod/items/custom/ScissorbladeItem.java
+++ src/main/java/io/github/jaminajar/jaminajarmod/items/custom/ScissorbladeItem.java
@@ -1,5 +1,11 @@
 package io.github.jaminajar.jaminajarmod.items.custom;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.util.ActionResult;
@@ -7,13 +13,32 @@ import net.minecraft.util.ActionResult;
 
 public class ScissorbladeItem extends ToolItem implements Vanishable {
 	static boolean ScissorMode = true;
+	/// I added a couple of fields mirroring the behavior of {@link SwordItem}.
+	private final float attackDamage;
+	private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
 
-	public ScissorbladeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+	/// The constructor now also mirrors what {@link SwordItem#SwordItem(ToolMaterial, int, float, Settings)  SwordItem} does.
+	/// You should ***definitely*** do this for your other custom weapons and tools as well.
+	public ScissorbladeItem(ToolMaterial material, int attackDamage, float attackSpeed, Item.Settings settings) {
 		super(material, settings);
+		this.attackDamage = (float)attackDamage + material.getAttackDamage();
+		ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+		builder.put(
+				EntityAttributes.GENERIC_ATTACK_DAMAGE,
+				new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", this.attackDamage, EntityAttributeModifier.Operation.ADDITION)
+		);
+		builder.put(
+				EntityAttributes.GENERIC_ATTACK_SPEED,
+				new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", attackSpeed, EntityAttributeModifier.Operation.ADDITION)
+		);
+		this.attributeModifiers = builder.build();
+
 	}
 
+	/// Added a couple of assertions and simplified the conditions here because they looked at me funny.
 	public ActionResult useOnBlock(ItemUsageContext context) {
 		PlayerEntity player = context.getPlayer();
+		assert player != null;
 		if (player.isSneaking()){
 			ModeChange();
 		}
@@ -22,11 +47,18 @@ public class ScissorbladeItem extends ToolItem implements Vanishable {
 	static void ModeChange(){
 		ScissorMode =!ScissorMode;
 		int j;
-		if(ScissorMode==true){
+		if(ScissorMode){
 			j = 2;
-		} else if(ScissorMode==false){
+		} else if(!ScissorMode){
 			j = 0;
 		}
+	}
+
+	/// I added this method because SwordItem seems to call it in order to actually apply the attributes
+	/// during runtime.
+	@Override
+	public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+		return slot == EquipmentSlot.MAINHAND ? this.attributeModifiers : super.getAttributeModifiers(slot);
 	}
 }
 
